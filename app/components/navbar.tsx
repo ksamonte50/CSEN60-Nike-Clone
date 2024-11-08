c4f5c609b1f19ee295990720e4191d737128cee2
ksamonte50 <ksamonte50@gmail.com>
2024-11-07 17:35:20 -0800

    finished navbar html

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -2,17 +2,35 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li><Link href="#">Jordan</Link></li>
-        <li><Link href="#">Converse</Link></li>
-      </ul>
-      <ul>
-        <li><Link href="#">Find a Store</Link></li>
-        <li><Link href="#">Help</Link></li>
-        <li><Link href="#">Join Us</Link></li>
-        <li><Link href="#">Sign In</Link></li>
-      </ul>
-    </nav>
+    <>
+        <nav className="flex">
+          <ul>
+            <li><Link href="#">Jordan</Link></li>
+            <li><Link href="#">Converse</Link></li>
+          </ul>
+          <ul>
+            <li><Link href="#">Find a Store</Link></li>
+            <li><Link href="#">Help</Link></li>
+            <li><Link href="#">Join Us</Link></li>
+            <li><Link href="#">Sign In</Link></li>
+          </ul>
+        </nav>
+        <nav className="flex">
+            <Link href="">Home</Link>
+            <ul>
+                <li><Link href="#">New</Link></li>
+                <li><Link href="#">Men</Link></li>
+                <li><Link href="#">Women</Link></li>
+                <li><Link href="#">Kids</Link></li>
+                <li><Link href="#">Jordan</Link></li>
+                <li><Link href="#">Sale</Link></li>
+            </ul>
+            <ul>
+                <li><input type="text" />Search</li>
+                <li><Link href="#">Favorites</Link></li>
+                <li><Link href="#">Cart</Link></li>
+            </ul>
+        </nav>
+    </>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Link from "next/link";
 
+import Navbar from "./components/navbar";
+
 export default function Home() {
   return (
     <>
-      
+      <Navbar />
+      <h1 className="text-5xl font-bold">Home</h1>
     </>
   );
 }
