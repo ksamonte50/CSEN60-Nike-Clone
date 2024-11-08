2cd3c0eee8cc0c55e4bc00872aa026c63e1cadf3
ksamonte50 <ksamonte50@gmail.com>
2024-11-07 17:57:09 -0800

    add navbar tailwind

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -3,21 +3,21 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <>
-        <nav className="flex">
-          <ul>
-            <li><Link href="#">Jordan</Link></li>
-            <li><Link href="#">Converse</Link></li>
+        <nav className="flex justify-between bg-gray-100">
+          <ul className="flex gap-3">
+            <li className=""><Link href="#">Jordan</Link></li>
+            <li className=""><Link href="#">Converse</Link></li>
           </ul>
-          <ul>
-            <li><Link href="#">Find a Store</Link></li>
-            <li><Link href="#">Help</Link></li>
-            <li><Link href="#">Join Us</Link></li>
-            <li><Link href="#">Sign In</Link></li>
+          <ul className="flex gap-3">
+            <li className=""><Link href="#">Find a Store</Link></li>
+            <li className=""><Link href="#">Help</Link></li>
+            <li className=""><Link href="#">Join Us</Link></li>
+            <li className=""><Link href="#">Sign In</Link></li>
           </ul>
         </nav>
-        <nav className="flex">
+        <nav className="flex justify-between">
             <Link href="">Home</Link>
-            <ul>
+            <ul className="flex gap-3">
                 <li><Link href="#">New</Link></li>
                 <li><Link href="#">Men</Link></li>
                 <li><Link href="#">Women</Link></li>
@@ -25,7 +25,7 @@ export default function Navbar() {
                 <li><Link href="#">Jordan</Link></li>
                 <li><Link href="#">Sale</Link></li>
             </ul>
-            <ul>
+            <ul className="flex gap-3">
                 <li><input type="text" />Search</li>
                 <li><Link href="#">Favorites</Link></li>
                 <li><Link href="#">Cart</Link></li>
--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,13 @@ export default function Home() {
         <p>Lebron doesn't duck pressure, he becomes it. The Lebron XXII is built to bring it.</p>
         <button>Explore</button>
       </section>
+      <section>
+        <h2>Ball With The Best</h2>
+        <div>
+          <button>Arrow Left</button>
+          <button>Arrow Right</button>
+        </div>
+      </section>
     </>
   );
 }
