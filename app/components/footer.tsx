70faa5b2786b001fecbaaa263cfc86a68bd45a23
ksamonte50 <ksamonte50@gmail.com>
2024-11-21 17:35:42 -0800

    footer styling

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,11 +1,21 @@
 import Link from "next/link";
-import { NikeIcon } from "./navbar";
+import Image from "next/image";
 
 export default function Footer() {
   return (
     <>
-        <Link href="#" className="flex justify-center"><NikeIcon /></Link>
-        <ul className="flex justify-center gap-x-8 pt-7 pb-9 font-medium">
+        <Link href="#" className="flex justify-center">
+            <Image 
+                data-qa="image-media-img" 
+                alt="Nike. Just Do It" 
+                src="https://static.nike.com/a/images/f_auto/dpr_2.0,cs_srgb/w_1695,c_limit/f8aaccc0-9f42-4956-9b6b-3112596a9d01/nike-just-do-it.png" 
+                data-landscape-url="https://static.nike.com/a/images/f_auto,cs_srgb/w_1920,c_limit/f8aaccc0-9f42-4956-9b6b-3112596a9d01/nike-just-do-it.png" data-portrait-url="https://static.nike.com/a/images/f_auto,cs_srgb/w_1920,c_limit/f8aaccc0-9f42-4956-9b6b-3112596a9d01/nike-just-do-it.png" 
+                data-image-loaded-class="guL_1FMX" 
+                width={2880}
+                height={60}
+            />
+        </Link>
+        <ul className="flex justify-center gap-x-8 pt-[26px] pb-[34px] mt-3 font-medium">
             <li className=""><Link href="#">Find a Store</Link></li>
             <li className=""><Link href="#">Help</Link></li>
             <li className=""><Link href="#">Join Us</Link></li>
@@ -52,42 +62,72 @@ export default function Footer() {
         <div className="flex justify-center m-auto mb-15 pt-12 border-b border-gray-200 w-[1599px]"></div>
         <nav className="flex justify-between p-12">
             <div>
-                <h3 className="text-xl mb-6">Featured</h3>
-                <ul>
-                    <li className="text-gray-500 font-medium mb-3"><Link href="#">Air Force 1</Link></li>
-                    <li className="text-gray-500 font-medium mb-3"><Link href="#">Jordan 1</Link></li>
-                    <li className="text-gray-500 font-medium mb-3"><Link href="#">Metcon</Link></li>
-                    <li className="text-gray-500 font-medium mb-3"><Link href="#">Air Max 270</Link></li>
+                <h4 className="text-sm font-medium mb-9">Resources</h4>
+                <ul className="">
+                    <li className="text-gray-500 text-sm font-medium mb-3"><Link href="#">Gift Cards</Link></li>
+                    <li className="text-gray-500 text-sm font-medium mb-3"><Link href="#">Find a Store</Link></li>
+                    <li className="text-gray-500 text-sm font-medium mb-3"><Link href="#">Membership</Link></li>
+                    <li className="text-gray-500 text-sm font-medium mb-3"><Link href="#">Nike Journal</Link></li>
+                    <li className="text-gray-500 text-sm font-medium mb-3"><Link href="#">Site Feedback</Link></li>
                 </ul>
             </div>
             <div>
-                <h3 className="text-xl mb-6">Shoes</h3>
-                <ul>
-                    <li className="text-gray-500 font-medium mb-3"><Link href="#">All Shoes</Link></li>
-                    <li className="text-gray-500 font-medium mb-3"><Link href="#">Jordan Shoes</Link></li>
-                    <li className="text-gray-500 font-medium mb-3"><Link href="#">Running Shoes</Link></li>
-                    <li className="text-gray-500 font-medium mb-3"><Link href="#">Basketball Shoes</Link></li>
+                <h4 className="text-sm font-medium mb-9">Help</h4>
+                <ul className="">
+                    <li className="text-gray-500 text-sm font-medium mb-3"><Link href="#">Get Help</Link></li>
+                    <li className="text-gray-500 text-sm font-medium mb-3"><Link href="#">Order Status</Link></li>
+                    <li className="text-gray-500 text-sm font-medium mb-3"><Link href="#">Shipping and Delivery</Link></li>
+                    <li className="text-gray-500 text-sm font-medium mb-3"><Link href="#">Returns</Link></li>
+                    <li className="text-gray-500 text-sm font-medium mb-3"><Link href="#">Order Cancellation</Link></li>
+                    <li className="text-gray-500 text-sm font-medium mb-3"><Link href="#">Payment Options</Link></li>
+                    <li className="text-gray-500 text-sm font-medium mb-3"><Link href="#">Gift Card Balance</Link></li>
+                    <li className="text-gray-500 text-sm font-medium mb-3"><Link href="#">Contact Us</Link></li>
                 </ul>
             </div>
             <div>
-                <h3 className="text-xl mb-6">Clothing</h3>
-                <ul>
-                    <li className="text-gray-500 font-medium mb-3"><Link href="#">All Clothing</Link></li>
-                    <li className="text-gray-500 font-medium mb-3"><Link href="#">Tops & T-Shirts</Link></li>
-                    <li className="text-gray-500 font-medium mb-3"><Link href="#">Shorts</Link></li>
-                    <li className="text-gray-500 font-medium mb-3"><Link href="#">Hoodies & Pullovers</Link></li>
+                <h4 className="text-sm font-medium mb-9">Company</h4>
+                <ul className="">
+                    <li className="text-gray-500 text-sm font-medium mb-3"><Link href="#">About Nike</Link></li>
+                    <li className="text-gray-500 text-sm font-medium mb-3"><Link href="#">News</Link></li>
+                    <li className="text-gray-500 text-sm font-medium mb-3"><Link href="#">Careers</Link></li>
+                    <li className="text-gray-500 text-sm font-medium mb-3"><Link href="#">Investors</Link></li>
+                    <li className="text-gray-500 text-sm font-medium mb-3"><Link href="#">Purpose</Link></li>
+                    <li className="text-gray-500 text-sm font-medium mb-3"><Link href="#">Sustainability</Link></li>
                 </ul>
             </div>
             <div>
-                <h3 className="text-xl mb-6">Kids</h3>
-                <ul>
-                    <li className="text-gray-500 font-medium mb-3"><Link href="#">Infant & Toddler Shoes</Link></li>
-                    <li className="text-gray-500 font-medium mb-3"><Link href="#">Kids Shoes</Link></li>
-                    <li className="text-gray-500 font-medium mb-3"><Link href="#">Kids Basketball Shoes</Link></li>
-                    <li className="text-gray-500 font-medium mb-3"><Link href="#">Kids Running Shoes</Link></li>
+                <h4 className="text-sm font-medium mb-9">Promotions and Discounts</h4>
+                <ul className="">
+                    <li className="text-gray-500 text-sm font-medium mb-3"><Link href="#">Students</Link></li>
+                    <li className="text-gray-500 text-sm font-medium mb-3"><Link href="#">Military</Link></li>
+                    <li className="text-gray-500 text-sm font-medium mb-3"><Link href="#">Teachers</Link></li>
+                    <li className="text-gray-500 text-sm font-medium mb-3"><Link href="#">First Responders & Medical Professionals</Link></li>
+                    <li className="text-gray-500 text-sm font-medium mb-3"><Link href="#">Birthday</Link></li>
                 </ul>
+            </div>
+            <div className="flex justify-end items-stretch">
+                <svg 
+                    aria-hidden="true" 
+                    focusable="false" 
+                    viewBox="0 0 24 24" 
+                    role="img" 
+                    width="24px" 
+                    height="24px" 
+                    fill="none"
+                    className="text-gray-500 w-4 mr-1"
+                >
+                    <path stroke="currentColor" strokeMiterlimit="10" strokeWidth="1.5" d="M21.75 12A9.75 9.75 0 0112 21.75M21.75 12A9.75 9.75 0 0012 2.25M21.75 12c0 2.071-4.365 3.75-9.75 3.75S2.25 14.071 2.25 12m19.5 0c0-2.071-4.365-3.75-9.75-3.75S2.25 9.929 2.25 12M12 21.75A9.75 9.75 0 012.25 12M12 21.75c2.9 0 5.25-4.365 5.25-9.75S14.9 2.25 12 2.25m0 19.5c-2.9 0-5.25-4.365-5.25-9.75S9.1 2.25 12 2.25M2.25 12A9.75 9.75 0 0112 2.25"></path>
+                </svg>
+                <h4 className="text-gray-500 text-sm font-medium mt-0.5">United States</h4>
             </div>
         </nav>
+        <ul>
+            <li><Link href="#">© 2024 Nike, Inc. All Rights Reserved</Link></li>
+            <li><Link href="#">© 2024 Nike, Inc. All Rights Reserved</Link></li>
+            <li><Link href="#">© 2024 Nike, Inc. All Rights Reserved</Link></li>
+            <li><Link href="#">© 2024 Nike, Inc. All Rights Reserved</Link></li>
+
+        </ul>
     </>
   );
 }
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -59,7 +59,7 @@ export function Heart() {
       fill="none"
       className="my-3"
       >
-        <path stroke="currentColor" stroke-width="1.5" d="M16.794 3.75c1.324 0 2.568.516 3.504 1.451a4.96 4.96 0 010 7.008L12 20.508l-8.299-8.299a4.96 4.96 0 010-7.007A4.923 4.923 0 017.205 3.75c1.324 0 2.568.516 3.504 1.451l.76.76.531.531.53-.531.76-.76a4.926 4.926 0 013.504-1.451"></path>
+        <path stroke="currentColor" strokeWidth="1.5" d="M16.794 3.75c1.324 0 2.568.516 3.504 1.451a4.96 4.96 0 010 7.008L12 20.508l-8.299-8.299a4.96 4.96 0 010-7.007A4.923 4.923 0 017.205 3.75c1.324 0 2.568.516 3.504 1.451l.76.76.531.531.53-.531.76-.76a4.926 4.926 0 013.504-1.451"></path>
       </svg>
   )
 }
@@ -76,7 +76,7 @@ export function Bag() {
       fill="none"
       className="my-3"
     >
-      <path stroke="currentColor" stroke-width="1.5" d="M8.25 8.25V6a2.25 2.25 0 012.25-2.25h3a2.25 2.25 0 110 4.5H3.75v8.25a3.75 3.75 0 003.75 3.75h9a3.75 3.75 0 003.75-3.75V8.25H17.5"></path>
+      <path stroke="currentColor" strokeWidth="1.5" d="M8.25 8.25V6a2.25 2.25 0 012.25-2.25h3a2.25 2.25 0 110 4.5H3.75v8.25a3.75 3.75 0 003.75 3.75h9a3.75 3.75 0 003.75-3.75V8.25H17.5"></path>
     </svg>
   )
 }
@@ -111,7 +111,7 @@ export default function Navbar() {
             </ul>
             <ul className="flex gap-3 items-center">
                 <li><button className="rounded-full p-1.5 bg-gray-100 hover:bg-gray-200">
-                  <svg aria-hidden="true" focusable="false" viewBox="0 0 24 24" role="img" width="24px" height="24px" fill="none"><path stroke="currentColor" stroke-width="1.5" d="M13.962 16.296a6.716 6.716 0 01-3.462.954 6.728 6.728 0 01-4.773-1.977A6.728 6.728 0 013.75 10.5c0-1.864.755-3.551 1.977-4.773A6.728 6.728 0 0110.5 3.75c1.864 0 3.551.755 4.773 1.977A6.728 6.728 0 0117.25 10.5a6.726 6.726 0 01-.921 3.407c-.517.882-.434 1.988.289 2.711l3.853 3.853"></path></svg>
+                  <svg aria-hidden="true" focusable="false" viewBox="0 0 24 24" role="img" width="24px" height="24px" fill="none"><path stroke="currentColor" strokeWidth="1.5" d="M13.962 16.296a6.716 6.716 0 01-3.462.954 6.728 6.728 0 01-4.773-1.977A6.728 6.728 0 013.75 10.5c0-1.864.755-3.551 1.977-4.773A6.728 6.728 0 0110.5 3.75c1.864 0 3.551.755 4.773 1.977A6.728 6.728 0 0117.25 10.5a6.726 6.726 0 01-.921 3.407c-.517.882-.434 1.988.289 2.711l3.853 3.853"></path></svg>
                 </button></li>
                 <li><input type="text" placeholder="Search" className="bg-gray-100 rounded-full pt-1.5 pb-1.5 pl-1 hover:bg-gray-200"/></li>
                 <li><Link href="#"><Heart /></Link></li>
