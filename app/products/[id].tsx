da37766a90635d30e894b098f897132beb4f1273
ksamonte50 <ksamonte50@gmail.com>
2024-11-22 15:27:15 -0800

    create product page

---
+++ app/products/[id].tsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+import Image from "next/image";
+
+export default function Product() {
+  return (
+    <>
+      <Navbar />
+      
+      <Footer />
+    </>
+  );
+}
