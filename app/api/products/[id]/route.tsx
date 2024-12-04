2be58a98a5f57da785a0d825a5cc144959612aea
ksamonte50 <ksamonte50@gmail.com>
2024-12-04 10:07:23 -0800

    changed context param

--- app/api/products/[id]/route.tsx
+++ app/api/products/[id]/route.tsx
@@ -1,8 +1,8 @@
 import { NextResponse, NextRequest } from 'next/server';
 import { query } from '@/lib/db';
 
-export async function GET (request: NextRequest, { params }: { params: { id: string } }) {
-    const { id } = params;
+export async function GET (request: NextRequest, context: { params: { id: string } }) {
+    const { id } = context.params;
 
     const product = await query('SELECT * FROM products WHERE id = $1', [id]);
     if (product.length === 0) {
