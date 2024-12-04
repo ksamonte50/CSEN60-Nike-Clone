9283a840e9a67e6cddb993b814ff772250500fb9
ksamonte50 <ksamonte50@gmail.com>
2024-12-04 10:00:13 -0800

    changed api request to nextrequest

--- app/api/products/[id]/route.tsx
+++ app/api/products/[id]/route.tsx
@@ -1,7 +1,7 @@
-import { NextResponse } from 'next/server';
+import { NextResponse, NextRequest } from 'next/server';
 import { query } from '@/lib/db';
 
-export async function GET (request: Request, { params }: { params: { id: string } }) {
+export async function GET (request: NextRequest, { params }: { params: { id: string } }) {
     const { id } = params;
 
     const product = await query('SELECT * FROM products WHERE id = $1', [id]);
